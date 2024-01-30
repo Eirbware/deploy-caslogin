bb357e4d3f745284b792c4d98d8ec111e3f57ece
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-01-30 19:13:15 +0100

    add root check

--- script.sh
+++ script.sh
@@ -1,14 +1,23 @@
 set -e
 
-echo -n "domain name: "
+if [ $(id -u) -ne 0 ]
+then 
+	echo "Please run as root" >&2
+	exit 1
+fi
+
+tar -xvzf setup.tgz
+
+echo -n "domain name (WITHOUT HTTP:// OR HTTPS://): "
 read DOMAIN_NAME
 export DOMAIN_NAME
 export CAS_DB_NAME="caslogin"
 export CAS_DB_USERNAME="caslogin"
 export CAS_DB_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16; echo)
 export API_KEY=$(cat /proc/sys/kernel/random/uuid)
+export COMPOSER_ALLOW_SUPERUSER=1
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPT_DIR
 
@@ -17,23 +26,27 @@ export LUCKPERMS_DB_USERNAME="luckperms"
 export LUCKPERMS_DB_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16; echo)
 
 apt update
-apt install git nginx openjdk-17-jre mariadb-server unzip zip curl php-fpm php-zip php-curl php-mysql -y
-. install_composer.sh
+apt install git git-lfs gettext nginx openjdk-17-jre mariadb-server unzip zip curl php-fpm php-zip php-curl php-mysql -y
+$SCRIPT_DIR/install_composer.sh
+
+cd $SCRIPT_DIR
 
 echo "Installing web server..."
-. web_server_install.sh
+$SCRIPT_DIR/web_server_install.sh
+
+cd $SCRIPT_DIR
 
 echo "Creating web server's database..."
-. web_db_install.sh
+$SCRIPT_DIR/web_db_install.sh
+
+cd $SCRIPT_DIR
+
+echo "Installing on nginx..."
+$SCRIPT_DIR/nginx_install.sh
+
+cd $SCRIPT_DIR
 
 echo "Installing MC Server..."
-. mc_server_install.sh
+$SCRIPT_DIR/mc_server_install.sh
 
-
-
-
-
-# export SERVER_NAME="paper"
-# envsubst < luckperms_config.yml.template
-# SERVER_NAME="proxy"
-# envsubst < luckperms_config.yml.template
+echo "FINISHED!"
